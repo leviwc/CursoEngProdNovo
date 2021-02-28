cbb23aa8e090ac4bf150983c9cc0d3e54f8048bf
leviwc <leviwc3@hotmail.com>
2021-02-27 23:47:05 -0300

    Updates

---
+++ precache-manifest.411c602a4e77256e512ea76fd56019da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc157a3aef93bc3705e30b6bff2aa9ba",
+    "url": "/CursoEngProdNovo/index.html"
+  },
+  {
+    "revision": "08547c98c271d59eaa60",
+    "url": "/CursoEngProdNovo/static/css/main.6e722a10.chunk.css"
+  },
+  {
+    "revision": "161f2c34e09e154dbe4c",
+    "url": "/CursoEngProdNovo/static/js/2.f9071a03.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/CursoEngProdNovo/static/js/2.f9071a03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08547c98c271d59eaa60",
+    "url": "/CursoEngProdNovo/static/js/main.e3e2471c.chunk.js"
+  },
+  {
+    "revision": "f3a182ac520e91a8ebbc",
+    "url": "/CursoEngProdNovo/static/js/runtime-main.7c13eede.js"
+  }
+]);
